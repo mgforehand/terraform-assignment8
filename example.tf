a8e92789c7845c19e2616f99cf65fcc12b13be2f
mgforehand <mgforehand@github.com>
2024-03-20 16:46:26 +0000

    3

--- example.tf
+++ example.tf
@@ -1,19 +1,18 @@
-#
-#terraform {
-#  required_providers {
-#    aws = {
-#      source  = "hashicorp/aws"
-#      version = "~> 5.38.0"
-#    }
-#  }
-#}
-#
-#provider "aws" {
-#  region  = "us-east-1"
-#}
-#
-#resource "aws_instance" "First-one" {
-#  #ami           = "ami-0c293f3f676ec4f90"
-#  ami           = "ami-0c7217cdde317cfec"
-#  instance_type = "t2.micro"
-#}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.38.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "First-one" {
+  #ami           = "ami-0c293f3f676ec4f90"
+  ami           = "ami-0c7217cdde317cfec"
+  instance_type = "t2.micro"
+}
