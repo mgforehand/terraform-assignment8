e46e7cda22ad69411b34e1a5120960f5fa843a63
mgforehand <mgforehand@github.com>
2024-03-20 17:11:55 +0000

    source

--- example.tf
+++ example.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.38.0"
+      source  = "hashicorp/terraform-provider-aws"
+      version = "~> 5.41.0"
     }
   }
 }
