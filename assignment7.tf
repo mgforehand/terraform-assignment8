b2ebb4c8bd3e0d4e3bd71941427bc6a8a3a39ec0
mgforehand <mgforehand@github.com>
2024-03-23 21:42:07 +0000

    fixes for the man

--- assignment7.tf
+++ assignment7.tf
@@ -1,4 +1,6 @@
-
+provider "aws" {
+  region = "us-east-1"
+}
 # Create a new VPC please work
 resource "aws_vpc" "tf-vpc" {
   cidr_block = "10.0.0.0/16"
--- example.tf
+++ example.tf
@@ -1,20 +1,10 @@
-# Provider code maybe1
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.42.0"
+  cloud {
+    organization = "mikesworld"
+
+    workspaces {
+      name = "terraform-assignment8"
     }
   }
 }
-
-provider "aws" {
-  # Configuration options 
-}
-    
-  
-resource "aws_instance" "First-one" {
-  #ami           = "ami-0c293f3f676ec4f90"
-  ami           = "ami-0c7217cdde317cfec"
-  instance_type = "t2.micro"
-}
+thats in example
