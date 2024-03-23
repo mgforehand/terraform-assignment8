2a600b18c9582bcca1f47f4bde90914082db358b
mgforehand <mgforehand@github.com>
2024-03-23 22:12:29 +0000

    fixes5

--- example.tf
+++ example.tf
@@ -1,13 +1,4 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.41.0"
-    }
-  }
-}
-
-terraform {
   cloud {
     organization = "mikesworld"
 
